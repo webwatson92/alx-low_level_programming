5f933959f6b0764a7b944f056fa4cd1dda19e3e0
Ouattara El Hadj Youssouf <webwatson92@gmail.com>
2022-03-16 00:47:18 +0100

    4-print_alphabet sauf q et e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
