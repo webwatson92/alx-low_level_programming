8ffb2eaa6e8fbbda810f028e27398f129e694236
Ouattara El Hadj Youssouf <webwatson92@gmail.com>
2022-03-15 02:56:52 +0100

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if(n>5)
-		printf("%d the string and is greater than 5\n", );
+		printf("%d the string and is greater than 5\n", n);
 	else if(n < 6 && n != 0)
-		printf("%d the string and is less than 6 and not 0\n", );
+		printf("%d the string and is less than 6 and not 0\n", n);
 	else
 		printf("the string and is 0\n");
 	return (0);
